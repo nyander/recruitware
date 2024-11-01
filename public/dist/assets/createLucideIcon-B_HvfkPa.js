c83feab63cd1c2c679cfbc69be8a7e432036dab4
Richard Nyande <rich.nyande@gmail.com>
2024-11-01 17:35:41 +0000

    http to https

--- public/dist/assets/createLucideIcon-B_HvfkPa.js
+++ public/dist/assets/createLucideIcon-B_HvfkPa.js
@@ -8,7 +8,7 @@ import{r as n}from"./app-DhQUmDYX.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */var f={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ */var f={xmlns:"https://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
  * @license lucide-react v0.428.0 - ISC
  *
  * This source code is licensed under the ISC license.
